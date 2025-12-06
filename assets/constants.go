6699d3a3e332b513d97d927bf20680343e643960
lookibed <93531352+lookibed@users.noreply.github.com>
2025-12-06 14:06:14 +0300

    Create constants.go

---
+++ assets/constants.go
@@ -0,0 +1,16 @@
+package assets
+
+// Asset folder constants, corresponding to C++ constants.hpp
+const (
+	ShadersFolder   = "shaders"
+	TexturesFolder  = "textures"
+	FontsFolder     = "fonts"
+	LayoutsFolder   = "layouts"
+	SoundsFolder    = "sounds"
+	ModelsFolder    = "models"
+	SkeletonsFolder = "skeletons"
+	PostEffectsFolder = "shaders/effects"
+)
+
+// Default font name
+const FontDefault = "normal"
